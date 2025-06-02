e14da73cfb5de6e3b58ca043f6b451a2cac74141
Julia Duffy <jtduffy7@gmail.com>
2025-06-02 15:05:29 -0600

    Implemented some of the backend logic, though it only works on the player side. Need to add in computer move logic

--- app/src/main/java/com/example/mancala/GameViewModel.kt
+++ app/src/main/java/com/example/mancala/GameViewModel.kt
@@ -1,17 +1,24 @@
 package com.example.mancala
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 // Store the position of marbles, scores, and game state for persistence
 class GameViewModel : ViewModel() {
 
     val playerScore = MutableStateFlow<Int>(0)
     val computerScore = MutableStateFlow<Int>(0)
-    var marbles = MutableStateFlow<List<Int>>(listOf(4,4,4,4,4,4, 4,4,4,4,4,4))
+    val marbles = MutableStateFlow<List<Int>>(listOf(4,4,4,4,4,4, 4,4,4,4,4,4))
     val isPlaying = MutableStateFlow<Boolean>(false)
     val currentPlayer = MutableStateFlow<Int>(0) // 0 is player 1 is computer
     val gameMode = MutableStateFlow<Int>(0) // 0 is easy 1 is med 2 is hard
+    private val moveMarbleEvent = MutableSharedFlow<Pair<Int,Int>>(extraBufferCapacity = 1)
+    private val playerCaptureEvent = MutableSharedFlow<Pair<Int,Int>>(extraBufferCapacity = 1)
+    private val moveInProgress = MutableStateFlow(false)
 
     // could add who plays first here eventually
     fun startGame(gameMode: String){
@@ -21,23 +28,112 @@ class GameViewModel : ViewModel() {
     /* callback for when a player clicks on a hole.
     1. If the hole contains no marbles, do nothing
     2. If the hole does have marbles, add 1 to the next x
-    indicies of the list, if x is the number of marbles
+    indices of the list
+    3. If hole + x = 7 and x == 1 break from the loop and add 1 to player score
+    4. If hole + x is between 1 - 6 and the hole across from hole + x has marbles, capture
+
+    Things that need to be returned by this method:
     */
     fun playerMove(hole: Int) {
-        val marbleCount = marbles.value[hole]
-        if (marbleCount == 0)
-            return
-        val mutableList = marbles.value.toMutableList()
-        for(i in 0..marbleCount) {
-            var holeToUpdate = hole + i % 12
-            mutableList[holeToUpdate] += 1
+        // check, then set moveInProgress to prohibit user interaction
+        if (moveInProgress.value) return
+        else moveInProgress.value = true
+        // Num marbles in hole that was clicked on
+        val initMarbleCount = marbles.value[hole]
+        // Num marbles left to distribute
+        var currMarbleCount = initMarbleCount
+        // Mutable copy of list of marble counts
+        val marblesCopy = marbles.value.toMutableList()
+        // Hole we are currently updating
+        var holeToUpdate = (hole + 1) % 14
+        // boolean to award extra turns
+        var extraTurn = false
+
+        viewModelScope.launch {
+            // play single marble animations
+            while (currMarbleCount > 1) {
+                // skip computer's store
+                if (holeToUpdate == 13) holeToUpdate = 0
+                // signal animation to move marble from one hole to another
+                moveMarbleEvent.emit(hole to holeToUpdate)
+                // allow time to play the animation
+                delay(300)
+                // increment next hole
+                marblesCopy[holeToUpdate]++
+                // decrement starting hole
+                marblesCopy[hole]--
+                // decrement marble count
+                currMarbleCount--
+                // update game stats
+                marbles.value = marblesCopy.toList()
+                // calculate hole to update
+                holeToUpdate = (holeToUpdate + 1) % 14
+            }
+
+            // on player turn, skip computer store
+            if (holeToUpdate == 13) holeToUpdate = 0
+
+            // if last marble goes in player store, award extra turn
+            if (holeToUpdate == 6) {
+
+                // animation
+                moveMarbleEvent.emit(hole to holeToUpdate)
+                delay(300)
+
+                // game state
+                marblesCopy[hole]--
+                marblesCopy[holeToUpdate]++
+                marbles.value = marblesCopy.toList()
+                playerScore.value += 1
+
+                // extra turn awarded (don't switch currPlayer)
+                moveInProgress.value = false
+                return@launch
+            }
+
+            // Check if it is a capture
+            else if (holeToUpdate in 0..5) {
+                val oppositeHole =  12 - holeToUpdate
+
+                // if opposite hole has marbles, play capture animation
+                if (marblesCopy[oppositeHole] > 0) {
+                    playerCaptureEvent.emit(holeToUpdate to oppositeHole)
+                    delay(300)
+                }
+
+                // update game stats
+                val capturedCount = 1 + marblesCopy[oppositeHole]
+                playerScore.value += capturedCount
+                marblesCopy[hole]--
+                marblesCopy[holeToUpdate] = 0
+                marblesCopy[oppositeHole] = 0
+                marbles.value = marblesCopy
+
+                // switch to the computer's turn
+                currentPlayer.value = 1
+                moveInProgress.value = false
+                return@launch
+            }
+            else {
+                // animation
+                moveMarbleEvent.emit(hole to holeToUpdate)
+                delay(200)
+
+                // update game state
+                marblesCopy[hole]--
+                marblesCopy[holeToUpdate]++
+                marbles.value = marblesCopy.toList()
+
+                // switch to computer's turn
+                currentPlayer.value = 1
+                moveInProgress.value = false
+                return@launch
+            }
         }
-
-
     }
-
-    fun computerMove(pit: Int, marbleCount: Int) {
-        // call method in
+    
+    fun calculateComputerMove() {
+        // call method in another class to get the next computer move
     }
 
     // for debugging

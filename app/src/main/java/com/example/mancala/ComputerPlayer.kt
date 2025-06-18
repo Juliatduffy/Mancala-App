645028a412a3e8afe07aa1879e2cf33cabe310ec
Julia Duffy <jtduffy7@gmail.com>
2025-06-17 19:31:08 -0600

    Accidentally lost a bunch of my work while traveling but thankfully had it backed up

--- app/src/main/java/com/example/mancala/ComputerPlayer.kt
+++ app/src/main/java/com/example/mancala/ComputerPlayer.kt
@@ -1,6 +1,8 @@
 package com.example.mancala
 
+import android.util.Log
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.withLock
 
 public class ComputerPlayer {
 
@@ -110,99 +112,149 @@ public class ComputerPlayer {
 
         // TODO implement hard algorithm (recursive)
         fun hard(boardState: List<Int>): Int {
-            return minimax(boardState, 1, 6).second
+            return minimax(boardState, 1, 3).second
         }
 
         // return best score, best move
         private fun minimax(boardState : List<Int>, currentPlayer : Int, depth: Int) : Pair<Int, Int> {
 
 //        If the game is over (one side has no marbles) or the depth <= 0
-//            return ai score - player score (maximizes ai score minimizes player score) and the best move
-//
+//        return ai score - player score (maximizes ai score minimizes player score) and the best move
+
             val playerOutOfMarbles= (0..5).all { boardState[it] == 0 }
             val computerOutOfMarbles = (7..12).all { boardState[it] == 0 }
-            if(playerOutOfMarbles || computerOutOfMarbles)
-                return evaluateScore(boardState) to -1 // placeholder move gets returned here
-            var bestScore = -10000
-            var bestMove = -1
 
-//        IF: if it is the AI's turn:
-//            - initialize the best score as -inf
-//            - foreach possible move:
-//                - copy the board
-//                - perform a move and see if the ai gets another turn
-//
-//            - if ai gets another turn:
-//                  call minimax again passing in the new board state with the ai as
-//                  the current player and the same depth
-//                  (we don't increment the depth until we switch turns because we
-//                  don't want to penalize the ai for doing something good (getting an extra turn))
-//
-//            - if no extra turn
-//                call minimax again with the player as current player and decrement the depth
-//
-//            If the resulting score is better than the current best:
-//                - update the best score and best move
-//        ELSE: If it's the PLAYER's turn:
-//            - initialize the best score as inf (very bad from the
-//              AI's perspective, anything worse than inf is better)
-//            - initialize best move as -1 or NONE
-//                - foreach move the PLAYER can make:
-//                    - copy the board
-//                    - perform each move
-//                    if results in an extra player turn, call minimax again with the same player and depth
-//
-//                    else: it doesn't result in an extra turn:
-//                        call minimax again with depth decremented and switch to ai player.
-//
-//        If the resulting score is better (less than for the player) than the current best:
-//               - update the best score and best move
-//
-//        After all moves return the best score and best move
+            // this symbolizes the bottom of the recursive tree
+            if(playerOutOfMarbles || computerOutOfMarbles || depth <= 0 )
+                return evaluateScore(boardState.toMutableList()) to -1 // placeholder move gets returned here
 
-
-            return 0 to 0
+            // if it is the ai turn
+            if(currentPlayer == 1) {
+                var bestResult = -10000 to -1
+                for(i in 7 .. 12) {
+                    if(boardState[i] == 0) continue
+                    var newResult = -10000 to -1
+                    val newBoard = boardState.toMutableList()
+                    // perform returns true if player gets extra move
+                    newResult = if (performMove(newBoard, i, currentPlayer)) {
+                        minimax(newBoard, currentPlayer, depth)
+                    }
+                    // no extra move
+                    else {
+                        minimax(newBoard, 0, depth -1)
+                    }
+                    if (newResult.first > bestResult.first)
+                        bestResult = newResult.first to i
+                }
+                return bestResult
+            }
+            else {
+                var bestResult = 10000 to -1
+                for(i in 0 .. 5) {
+                    if(boardState[i] == 0) continue
+                    var newResult = -10000 to -1
+                    val newBoard = boardState.toMutableList()
+                    // perform returns true if player gets extra move
+                    newResult = if (performMove(newBoard, i, currentPlayer)) {
+                        minimax(newBoard, currentPlayer, depth) // don't decrement depth here
+                    }
+                    else {
+                        minimax(newBoard, 0, depth -1)
+                    }
+                    if (newResult.first < bestResult.first)
+                        bestResult = newResult.first to i
+                }
+                return bestResult
+            }
         }
 
-        private fun evaluateScore(boardState: List<Int>): Any {
-            //TODO calculate last move scores
+        private fun performMove(boardCopy: MutableList<Int>, hole: Int, currentPlayer: Int) : Boolean {
 
+            var marbleCount = boardCopy[hole]
+            var holeToUpdate = hole
+
+            // MOVE ALL MARBLES EXCEPT THE LAST ONE -----------------------------
+            // place in following pits
+            while (marbleCount > 1) {
+                holeToUpdate = (holeToUpdate + 1) % 14
+                // on player turn, skip computer store
+                if (currentPlayer == 0 && holeToUpdate == 13) holeToUpdate = 0
+                // on computer turn skip player store
+                else if (currentPlayer == 1 && holeToUpdate == 6) holeToUpdate = 7
+                // increment next hole
+                boardCopy[holeToUpdate]++
+                // decrement starting hole
+                boardCopy[hole]--
+                // decrement marble count
+                marbleCount--
+                // calculate hole to update
+            }
+
+            // MOVE LAST MARBLE AND CALCULATE FINAL SCORES -------------------------
+            // on player turn, skip computer store
+            if (currentPlayer == 0 && holeToUpdate == 13) holeToUpdate = 0
+            // on computer turn skip player store
+            if (currentPlayer == 1 && holeToUpdate == 6) holeToUpdate = 7
+            // Calculate opposite hole for capture scenarios
+            val oppositeHole = 12 - holeToUpdate
+
+            // CASE 1: LAST MARBLE LANDS IN PLAYER STORE (AND IT IS PLAYER'S TURN), EXTRA TURN IS AWARDED
+            if (holeToUpdate == 6) {
+                // game state
+                boardCopy[holeToUpdate]++
+                boardCopy[hole]--
+                return true
+            }
+            // CASE 2: LAST MARBLE LANDS IN COMPUTER STORE,(AND IT IS COMPUTER'S TURN), EXTRA TURN IS AWARDED
+            else if (holeToUpdate == 13) {
+                boardCopy[holeToUpdate]++
+                boardCopy[hole]--
+                return true
+            }
+            // CASE 3: PLAYER SIDE CAPTURE, SWITCH TURNS
+            else if (holeToUpdate in 0..5 && boardCopy[holeToUpdate] == 0 && boardCopy[oppositeHole] > 0 && currentPlayer == 0) {
+                // update game stats
+                val capturedCount = 1 + boardCopy[oppositeHole]
+                boardCopy[6] += capturedCount
+                boardCopy[hole] = 0
+                boardCopy[holeToUpdate] = 0
+                boardCopy[oppositeHole] = 0
+                return false
+            }
+
+            // CASE 4: COMPUTER SIDE CAPTURE, SWITCH TURNS
+            else if (holeToUpdate in 7..12 && boardCopy[holeToUpdate] == 0 && boardCopy[oppositeHole] > 0 && currentPlayer == 1) {
+                // update game stats
+                val capturedCount = 1 + boardCopy[oppositeHole]
+                boardCopy[13] += capturedCount
+                boardCopy[hole] = 0
+                boardCopy[holeToUpdate] = 0
+                boardCopy[oppositeHole] = 0
+                return false
+            }
+
+            // CASE 5: NORMAL LAST MARBLE PLACEMENT, SWITCH TURNS
+            else {
+                Log.d( "ComputerPlayer", "$boardCopy")
+                boardCopy[holeToUpdate]++
+                boardCopy[hole]--
+                return false
+            }
         }
 
-//        private fun checkForWin(marblesCopy : MutableList<Int>) {
-//            val playerOutOfMarbles= (0..5).all { marblesCopy[it] == 0 }
-//            val computerOutOfMarbles = (7..12).all { marblesCopy[it] == 0 }
-//
-//            if (!playerOutOfMarbles && !computerOutOfMarbles)
-//                return
-//
-//            viewModelScope.launch {
-//                if (playerOutOfMarbles) {
-//                    for (i in 7 .. 12) {
-//                        for (j in 0 until marblesCopy[i]) {
-//                            _moveMarbleEvent.emit(i to 13)
-//                            marblesCopy[i]--
-//                            marblesCopy[13]++
-//                            _computerScore.value++
-//                            _marbles.value = marblesCopy.toList()
-//                        }
-//                    }
-//                }
-//                if (computerOutOfMarbles) {
-//                    for (i in 0 .. 5) {
-//                        for (j in 0 until marblesCopy[i]) {
-//                            _moveMarbleEvent.emit(i to 6)
-//                            marblesCopy[i]--
-//                            marblesCopy[6]++
-//                            _playerScore.value++ // TODO get rid of play and computer score and just reference the marbles array indices 6 and 13
-//                            _marbles.value = marblesCopy.toList()
-//                        }
-//                    }
-//                }
-//                val winner = if (marblesCopy[6] > marblesCopy[13]) 0 else 1
-//                _winEvent.emit(winner)
-//            }
+        // returns ai score - player score
+        private fun evaluateScore(board: List<Int>): Int {
+            val playerStore    = board[6]
+            val computerStore  = board[13]
+            val playerPitsSum  = board.subList(0, 6).sum()
+            val computerPitsSum= board.subList(7, 13).sum()
 
+            // “Material” on board + store
+            val playerTotal    = playerStore   + playerPitsSum
+            val computerTotal  = computerStore + computerPitsSum
 
+            // Positive means good for computer
+            return (computerTotal - playerTotal)
+        }
     }
 }

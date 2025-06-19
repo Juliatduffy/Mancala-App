dd7378273c3776421a68afdbcc617fdee0a1d644
Julia Duffy <jtduffy7@gmail.com>
2025-06-19 16:13:49 -0400

    Added arguments to settings navigation so I can implement first move selection

--- app/src/main/java/com/example/mancala/SettingsFragment.kt
+++ app/src/main/java/com/example/mancala/SettingsFragment.kt
@@ -27,7 +27,8 @@ class SettingsFragment: Fragment() {
         val nav = findNavController()
 
         binding.backButtonSettings.setOnClickListener {
-            nav.navigate(R.id.homeFragment)
+            val args = Bundle().apply { "firstMove" to binding.firstMoveSlider.value.toString() }
+            nav.navigate(R.id.action_settings_to_home, args)
         }
     }
 
